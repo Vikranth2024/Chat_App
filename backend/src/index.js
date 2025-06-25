cfa42788d83a6e798c98f4c4934e75b1ff4a14cd
Vikranth2024 <vikranthsabari436@gmail.com>
2025-06-25 10:48:38 +0530

    completed

---
+++ backend/src/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import { connectDB } from "./lib/db.js";
+dotenv.config();
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js";
+
+import { app, server } from "./lib/socket.js";
+
+// Increase payload size limits
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+
+// CORS middleware (must be before routes)
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use('/api/auth', authRoutes)
+app.use('/api/messages', messageRoutes)
+
+connectDB()
+server.listen(5001,() => {
+    console.log("server running on port http://localhost:5001")
+    
+})
