2f952ce6e059b095a26c6cc00402ab5f943f3489
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2025-08-13 16:56:31 +0530

    Update useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://chat-app-gray-seven.vercel.app";
+const SOCKET_URL = "https://chat-app-srbx.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -102,3 +102,4 @@ export const useAuthStore = create((set, get) => ({
   },
 
 }));
+
