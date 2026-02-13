5d16037b3d0dbb91315186369be1ff5cf9261e6d
Vikranth2024 <167829581+Vikranth2024@users.noreply.github.com>
2026-02-13 16:28:30 +0530

    Update ai.js

--- backend/src/lib/ai.js
+++ backend/src/lib/ai.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyCDDHDbNnQV9wDmxHTWljlclABhMVwwNwk");
 
 const languageMap = {
   "en": "English",
